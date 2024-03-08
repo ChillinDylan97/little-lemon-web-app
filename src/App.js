975f70719d84b3e40ddc5a583a121fa14850477f
ChillinDylan97 <dylangilbert601@gmail.com>
2024-03-07 20:15:48 -0600

    Update 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import './App.css';
+import Nav from  '/.components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Nav/>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
   );
 }
 
