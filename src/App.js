0e79db358016465d958686cc0878273f7e48700c
ChillinDylan97 <dylangilbert601@gmail.com>
2024-03-07 22:01:19 -0600

    Update 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
-import Nav from  '/.components/Nav';
+import Nav from  './components/Nav';
+
 
 function App() {
   return (
     <>
       <Nav/>
-      <Header/>
-      <Main/>
-      <Footer/>
+      
     </>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,9 +4,39 @@ import logo from '../images/Logo .svg'
 const Nav=()=> {
     return (
         <nav>
-            <a href="/">
-                <img src=''/>
+            <a href='/'>
+                <img src={logo}/>
             </a>
+            <div>
+                <div className='bar'></div>
+                <div className='bar'></div>
+                <div className='bar'></div>
+            </div>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/">About</a>
+                </li>
+                <li>
+                    <a href="/">Services</a>
+                </li>
+                <li>
+                    <a href="/">Menu</a>
+                </li>
+                <li>
+                    <a href="/">Reservations</a>
+                </li>
+                <li>
+                    <a href="/">Order Online</a>
+                </li>
+                <li>
+                    <a href="/">Login</a>
+                </li>
+            </ul>
         </nav>
-    )
+    );
 };
+
+export default Nav;
