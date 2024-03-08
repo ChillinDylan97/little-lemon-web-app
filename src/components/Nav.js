07af26bda5ce3e3054c17d91201b1ad1d0624c34
ChillinDylan97 <dylangilbert601@gmail.com>
2024-03-07 23:08:47 -0600

    CSS and navbar complete

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from '../images/Logo .svg'
 
-const Nav=()=> {
+    const Nav=()=>{
+        const [menuOpen,setMenuOpen]=useState(false);
+        const toggleMenu=()=>{setMenuOpen(!menuOpen);
+        }
     return (
-        <nav>
-            <a href='/'>
-                <img src={logo}/>
+        <nav className={` navbar ${menuOpen ? "open" : ""}`}>
+            <a href='/' className="logo">
+            <img src={logo}/>
             </a>
-            <div>
+            <div className="menu-icon" onClick={toggleMenu}>
                 <div className='bar'></div>
                 <div className='bar'></div>
                 <div className='bar'></div>
             </div>
-            <ul>
+            <ul className={`nav-links ${menuOpen ? "visible":""}`}>
                 <li>
                     <a href="/">Home</a>
                 </li>
